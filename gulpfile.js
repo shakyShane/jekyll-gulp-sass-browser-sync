6d2bc2ca7eef80ebe2de67a339d3654189223284
Seth Coelen <seth@coelen.co>
2015-09-23 17:36:28 -0400

    watches all html files in the root for changes

--- gulpfile.js
+++ gulpfile.js
@@ -56,7 +56,7 @@ gulp.task('sass', function () {
  */
 gulp.task('watch', function () {
     gulp.watch('_scss/*.scss', ['sass']);
-    gulp.watch(['index.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
 /**
