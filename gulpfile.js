5c28ac2e6b5fa92c4578ed5cb699e61480b94184
Yahya J Aifit <yahya@yja.me>
2019-08-21 20:21:48 +0700

    init gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,101 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var sass        = require('gulp-sass');
-var prefix      = require('gulp-autoprefixer');
-var cp          = require('child_process');
+const { src, dest, series, watch } = require('gulp'),
+  browserSync = require('browser-sync'),
+  sass        = require('gulp-sass'),
+  prefix      = require('gulp-autoprefixer'),
+  sourcemaps  = require('gulp-sourcemaps');
 
-var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
-var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+const config = {
+  src: '_scss/',
+  build: 'css/',
 };
 
-/**
- * Build the Jekyll Site
- */
-gulp.task('jekyll-build', function (done) {
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
-        .on('close', done);
-});
+function jekyll() {
+  return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit" });
+}
 
 /**
  * Rebuild Jekyll & do page reload
  */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-    browserSync.reload();
-});
+function jekyllReload(done) {
+  browserSync.reload();
+  watch(config.src, jekyllSass)
+  done();
+}
+// gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+//   browserSync.reload();
+// });
 
 /**
  * Wait for jekyll-build, then launch the Server
  */
-gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
-    browserSync({
-        server: {
-            baseDir: '_site'
-        }
-    });
-});
+
+function jekyllSass() {
+  return src(config.src)
+    // .pipe(plumber())
+    .pipe(sass({
+      outputStyle: 'compact',
+      includePaths: [
+        'node_modules',
+      ]
+    }))
+
+    .pipe(prefix({
+      browsers: ['last 3 versions']}))
+    .pipe(sourcemaps.write())
+    .pipe(dest(config.build))
+    .pipe(browserSync.stream())
+}
+
+function serve(done) {
+  browserSync.init({
+    server: {
+      baseDir: config.pugout
+    },
+    port: 4000
+  });
+  watch(config.src, jekyllSass)
+  watch(config.build, jekyllReload)
+  done();
+}
+// gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
+//   browserSync({
+//     server: {
+//       baseDir: '_site'
+//     }
+//   });
+// });
 
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
-gulp.task('sass', function () {
-    return gulp.src('_scss/main.scss')
-        .pipe(sass({
-            includePaths: ['scss'],
-            onError: browserSync.notify
-        }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/css'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('css'));
-});
+// gulp.task('sass', function () {
+//   return gulp.src('_scss/main.scss')
+//   .pipe(sass({
+//     includePaths: ['scss'],
+//     onError: browserSync.notify
+//   }))
+//   .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+//   .pipe(gulp.dest('_site/css'))
+//   .pipe(browserSync.reload({stream:true}))
+//   .pipe(gulp.dest('css'));
+// });
 
 /**
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
-gulp.task('watch', function () {
-    gulp.watch('_scss/*.scss', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
-});
+// gulp.task('watch', function () {
+//   gulp.watch('_scss/*.scss', ['sass']);
+//   gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
+// });
 
 /**
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['browser-sync', 'watch']);
+// gulp.task('default', ['browser-sync', 'watch']);
+
+const build = series(jekyllSass, jekyll);
+exports.build = build;
+
+exports.default = series(serve);
